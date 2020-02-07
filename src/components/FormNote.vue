e4f40493f1884ac74fd4d3103422c45a5192efa6
celvineadiputra <celvineadiputra@mhs.mdp.ac.id>
2020-02-07 15:16:08 +0700

    Init 1 perbaikan tampilan

--- src/components/FormNote.vue
+++ src/components/FormNote.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-        form
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-sm-12 d-flex justify-content-end">
+                <button class="btn btn-success mr-2">Save</button>
+                <button class="btn btn-danger mr-2">Delete</button>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-sm-12">
+                <div class="form-group">
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
